1c19a1de3e207d8575255f176822b6a32e7d782b
taesikyoon-aponati <tsyoon@uponati.com>
2024-10-16 14:42:25 +0900

    developer 계정 생성 시 권한 700 부여 추가적인 계정이 생길 시 ssh 접속이 안되며, 안되는 이유는 보안상 ssh 폴더의 권한이 700으로 설정되어야함(그룹이 실행권한을 가지면 안됨) 공유 폴더를 홈에 새로 생성함으로 해당 폴더에서 작업하도록 해야함

--- ncp_scripts/setup.sh
+++ ncp_scripts/setup.sh
@@ -41,8 +41,10 @@ sudo systemctl restart sshd
 sudo adduser developer --disabled-password --gecos ""
 echo "developer:$password" | sudo chpasswd
 echo "developer ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/init-developer
-sudo chmod 2770 /home/developer  # 그룹 쓰기 권한 및 setgid 설정 (developer 그룹에 속한 사용자가 읽기/쓰기/실행 가능)
-sudo chmod g+s /home/developer    # setgid 비트 설정으로 파일/폴더 생성 시 그룹 상속
+sudo chmod 700 /home/developer
+sudo mkdir -p /home/shared_directory
+sudo chown developer:developer /home/shared_directory
+sudo chmod 2770 /home/shared_directory  # 그룹 쓰기 권한 및 setgid 설정
 
 # SSH 키 생성 및 권한 설정
 sudo mkdir -p /home/developer/.ssh
